c647d898ec387f415b635407ae6ef3ea8675f903
TalhaJubaerPrantor <talhajubaer3121@gmail.com>
2024-12-21 23:57:46 +0600

    fixing bug for deploy

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "./navbar.css";
 import logo from "../../Images/shauqh-logo.jpg"
